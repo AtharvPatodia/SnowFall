7e76f07e2d719aa42a37cdf5c1b0335051942246
AtharvPatodia <75477499+AtharvPatodia@users.noreply.github.com>
2021-06-10 14:36:54 +0530

    Add files via upload

---
+++ Snow.js
@@ -0,0 +1,20 @@
+class Snow{
+    constructor(x, y, width, height){
+        var options={
+            'frictionAir': 0.005,
+            'density': 1,
+            isStatic: false
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = 10;
+        this.height = 10;
+        this.image = loadImage('snow4.webp');
+        World.add(world, this.body);
+    }
+    display(){
+        var position = this.body.position
+        rectMode(CENTER);
+        this.imageMode(CENTER);
+        this.image(snow_image, random(5, 695), 0, this.width, this.height);
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,22 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+var bg;
+function setup() {
+  createCanvas(1200,700);
+  engine = Engine.create();
+  world = engine.world;
+  bg = loadImage("snow3.jpg");
+}
+
+function draw() {
+  background(bg); 
+  spawnSnow(); 
+  drawSprites();
+}
+function spawnSnow(){
+  if(frameCount % 50 === 0){
+    var snow = new Snow(Math.round(random(5, 695)))
+    snow.lifetime = 1000; 
+  }
+}
